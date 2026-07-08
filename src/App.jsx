878ca6afe9feab7977f20d9e8dea9d3a09248490
Farick <farickterancusi@gmail.com>
2026-07-08 18:45:00 -0500

    Commit Inicial - Crear proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Gestión de Eventos Universitarios</h1>
+
+      <p>
+        Bienvenido a la plataforma para gestionar eventos académicos.
+      </p>
+    </div>
+  );
+}
+
+export default App;
